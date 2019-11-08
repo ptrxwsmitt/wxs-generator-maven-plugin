901bad8c40f4e8b19cd77f80a04e9eae21ea65c4
Patrick Waldschmitt <patrick.waldschmitt@gmail.com>
2019-11-08 11:12:24 +0100

    some cleanup

--- src/test/kotlin/wxsgen/VersionHelperTest.kt
+++ src/test/kotlin/wxsgen/VersionHelperTest.kt
@@ -9,36 +9,36 @@ class VersionHelperTest {
 
     @Test
     fun isVersionValidTest() {
-        assertThat(isVersionValid("1")).isTrue
-        assertThat(isVersionValid("1-SNAPSHOT")).isTrue
-        assertThat(isVersionValid("10")).isTrue
-        assertThat(isVersionValid("1a")).isFalse
-        assertThat(isVersionValid("a")).isFalse
-        assertThat(isVersionValid(".")).isFalse
-        assertThat(isVersionValid("1.")).isFalse
-        assertThat(isVersionValid("1.2")).isTrue
-        assertThat(isVersionValid("1.b")).isFalse
-        assertThat(isVersionValid("1.2b")).isFalse
-        assertThat(isVersionValid("1.b2")).isFalse
-        assertThat(isVersionValid("1.2-SNAPSHOT")).isTrue
-        assertThat(isVersionValid("1.2-SNAPSHOT-buildXYZ")).isTrue
-        assertThat(isVersionValid("11.22.33")).isTrue
-        assertThat(isVersionValid("11.22.33.44")).isTrue
-        assertThat(isVersionValid("11.22.33.44.55")).isTrue
-        assertThat(isVersionValid("11.22.33.44.55-SNAPSHOT")).isTrue
-        assertThat(isVersionValid("11.22.33.44.55.66")).isFalse
-        assertThat(isVersionValid("11.22.33.44.55.66-SNAPSHOT")).isFalse
-        assertThat(isVersionValid("2-SNAPSHOT")).isTrue
-        assertThat(isVersionValid("2-SNAPSHOT-buildxyz")).isTrue
-        assertThat(isVersionValid("2--SOMETHING")).isTrue
-        assertThat(isVersionValid("1.")).isFalse
-        assertThat(isVersionValid("10.")).isFalse
-        assertThat(isVersionValid("1.1.")).isFalse
-        assertThat(isVersionValid("a.b.c")).isFalse
-        assertThat(isVersionValid("1a.b.c")).isFalse
-        assertThat(isVersionValid("1a.2b.c")).isFalse
-        assertThat(isVersionValid("2.b2.c")).isFalse
-        assertThat(isVersionValid("2.2b")).isFalse
+        assertThat(isVersionValid("1")).isTrue()
+        assertThat(isVersionValid("1-SNAPSHOT")).isTrue()
+        assertThat(isVersionValid("10")).isTrue()
+        assertThat(isVersionValid("1a")).isFalse()
+        assertThat(isVersionValid("a")).isFalse()
+        assertThat(isVersionValid(".")).isFalse()
+        assertThat(isVersionValid("1.")).isFalse()
+        assertThat(isVersionValid("1.2")).isTrue()
+        assertThat(isVersionValid("1.b")).isFalse()
+        assertThat(isVersionValid("1.2b")).isFalse()
+        assertThat(isVersionValid("1.b2")).isFalse()
+        assertThat(isVersionValid("1.2-SNAPSHOT")).isTrue()
+        assertThat(isVersionValid("1.2-SNAPSHOT-buildXYZ")).isTrue()
+        assertThat(isVersionValid("11.22.33")).isTrue()
+        assertThat(isVersionValid("11.22.33.44")).isTrue()
+        assertThat(isVersionValid("11.22.33.44.55")).isTrue()
+        assertThat(isVersionValid("11.22.33.44.55-SNAPSHOT")).isTrue()
+        assertThat(isVersionValid("11.22.33.44.55.66")).isFalse()
+        assertThat(isVersionValid("11.22.33.44.55.66-SNAPSHOT")).isFalse()
+        assertThat(isVersionValid("2-SNAPSHOT")).isTrue()
+        assertThat(isVersionValid("2-SNAPSHOT-buildxyz")).isTrue()
+        assertThat(isVersionValid("2--SOMETHING")).isTrue()
+        assertThat(isVersionValid("1.")).isFalse()
+        assertThat(isVersionValid("10.")).isFalse()
+        assertThat(isVersionValid("1.1.")).isFalse()
+        assertThat(isVersionValid("a.b.c")).isFalse()
+        assertThat(isVersionValid("1a.b.c")).isFalse()
+        assertThat(isVersionValid("1a.2b.c")).isFalse()
+        assertThat(isVersionValid("2.b2.c")).isFalse()
+        assertThat(isVersionValid("2.2b")).isFalse()
     }
 
     @Test
